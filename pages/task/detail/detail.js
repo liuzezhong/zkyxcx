7f6cbf6a2643cb640a0c739f243e370c1876779c
liuzezhong <zhongliuze@gmail.com>
2017-07-20 15:05:46 +0800

    2017-07-20

--- pages/task/detail/detail.js
+++ pages/task/detail/detail.js
@@ -5,14 +5,59 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    tasksInfo: {},
+    project_value: [],
+    project_key: [],
+    array: ['美国', '中国', '巴西', '日本'],
+    index: 0,
+    tasks_id: 0,
+    objectArray: [
+      {
+        id: 0,
+        name: '美国'
+      },
+      {
+        id: 1,
+        name: '中国'
+      },
+      {
+        id: 2,
+        name: '巴西'
+      },
+      {
+        id: 3,
+        name: '日本'
+      }
+    ],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var tasks_id = options.tasks_id;
+    var that = this;
+    that.setData({
+      'tasks_id': tasks_id,
+    });
+    wx.request({
+      url: 'http://localhost/index.php?m=activity&c=index&a=getActivityDetails', //仅为示例，并非真实的接口地址
+      data: {
+        tasks_id: tasks_id,
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      method: 'POST',
+      success: function (res) {
+        console.log(res.data);
+        that.setData({
+          'tasksInfo': res.data.tasks,
+          'project_value': res.data.project_value,
+          'project_key': res.data.project_key,
+        });
+      },
+    })
   },
 
   /**
@@ -62,5 +107,18 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+
+  bindPickerChange: function (e) {
+    var tasks_id = this.data.tasks_id;
+    console.log('picker发送选择改变，携带值为', e.detail.value);
+    this.setData({
+      index: e.detail.value
+    });
+    var project_id = this.data.project_key[this.data.index];
+    console.log(project_id);
+    wx.navigateTo({
+      url: '/pages/task/form/form?tasks_id='+tasks_id+'&project_id='+project_id,
+    })
+  },
 })
